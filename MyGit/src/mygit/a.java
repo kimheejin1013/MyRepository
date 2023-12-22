a15930f47b54eefc0225d46d0cfdf0993a9878a9
kimhe <kimhe@192.168.219.106>
2023-12-22 21:55:12 +0900

    a change in master branch

--- MyGit/src/mygit/a.java
+++ MyGit/src/mygit/a.java
@@ -5,6 +5,7 @@ public class a {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("This is the 2nd version.");
+		System.out.println("a.java changed in master branch.");
 	}
 
 }
