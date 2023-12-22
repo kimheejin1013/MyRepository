80ae4073d689c5896a74a5222d798f8b89dda072
kimhe <kimhe@192.168.219.106>
2023-12-22 21:52:07 +0900

    First commit in devB branch

--- MyGit/src/mygit/a.java
+++ MyGit/src/mygit/a.java
@@ -5,6 +5,8 @@ public class a {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("This is the 2nd version.");
+		b your = new b();
+		your.sayHello("Everyone");
 	}
 
 }
